858b651877f8fc428a47598b423132954ad98f31
woojoung <ywj981217@naver.com>
2024-10-30 14:04:12 +0900

    Chore : Intersection observer 최적화

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,22 +1,37 @@
 /** @jsxImportSource @emotion/react */
-import React from 'react';
+import React, { Suspense } from 'react';
 import HomePageTitle from '@components/home/HomePageTitle';
 import Footer from '@components/common/Footer';
 import styled from '@emotion/styled';
 import CoupleInfomation from '@components/home/CoupleInfomation';
-const OurReport = React.lazy(() => import('@components/home/OurReport'));
+import { useInView } from 'react-intersection-observer';
+
 //  홈에서 쓰는 컴포넌트
 //<HomePageTitle /> = 제목 컴포넌트
 //<CoupleInfomation /> = 부부연동 ,미션
 //<Footer> = 푸터는 메인에서만 필요해서 호출
 
+const OurReport = React.lazy(() => import('@components/home/OurReport'));
+
 const Home = () => {
+  const { ref, inView } = useInView({
+    triggerOnce: true, // 한 번만 로드
+    threshold: 1, // 컴포넌트의 100%가 보일 때 로드
+    rootMargin: '50px', // 컴포넌트가 완전히 보일 때까지 기다림
+  });
+
   return (
     <HompageContainer>
       <ContentWrapper>
         <HomePageTitle />
         <CoupleInfomation />
-        <OurReport />
+        <div ref={ref}>
+          {inView && (
+            <Suspense fallback={<div>Loading...</div>}>
+              <OurReport />
+            </Suspense>
+          )}
+        </div>
       </ContentWrapper>
       <Footer />
     </HompageContainer>
