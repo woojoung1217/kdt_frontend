93760d5ef460d345f961355bcb57ecbacd7bd28f
kyungmim <kyungmin727@gmail.com>
2024-11-04 15:57:45 +0900

    Feat:스레스 척도 검사 결과 UI 구현

--- src/pages/scale/ScaleResult.tsx
+++ src/pages/scale/ScaleResult.tsx
@@ -1,46 +1,61 @@
-import EChartsReact from 'echarts-for-react';
-
-const data = [{ essential: '26', refusing: '22', relational: '24', sexual: '24', social: '24', total: '120' }];
+/** @jsxImportSource @emotion/react */
+import Button from '@components/common/Button';
+import { css } from '@emotion/react';
+import variables from '@styles/Variables';
+// import { useNavigate } from 'react-router-dom';
+import ScaleTotalGraph from './ScaleTotalGraph';
+import ScaleTypeGraph from './ScaleTypeGraph';
 
 const ScaleResult = () => {
-  const option = {
-    angleAxis: {
-      max: 30,
-    },
-    radiusAxis: {
-      type: 'category',
-      data: ['essential', 'refusing', 'relational', 'sexual', 'social'],
-      z: 10,
-    },
-    polar: {},
-    series: [
-      {
-        name: 'Scale',
-        type: 'bar',
-        data: [
-          `${data[0].essential}`,
-          `${data[0].refusing}`,
-          `${data[0].relational}`,
-          `${data[0].sexual}`,
-          `${data[0].social}`,
-        ],
-        coordinateSystem: 'polar',
-      },
-    ],
-    legend: {},
-  };
+  // const navigate = useNavigate();
 
   return (
     <>
-      <div>
-        <h2>희선님의 난임 스트레스 척도 분포도</h2>
+      <div css={BackGroundColor}>
+        <div css={Title}>
+          <h2>희선님의 난임 스트레스 척도분포도</h2>
 
-        <p>난임 스트레스 검사 기준으로 분류되었어요</p>
+          <p>난임 스트레스 검사 기준으로 분류되었어요</p>
+        </div>
+
+        <ScaleTotalGraph />
+        <ScaleTypeGraph />
+
+        <Button
+          text="스트레스 척도 리스트로 돌아가기 "
+          // onClick={() => navigate('/scale/result')}
+          disabled={false}
+          type="button"
+          size="large"
+        />
       </div>
-
-      <EChartsReact option={option} opts={{ renderer: 'svg', width: 'auto', height: 100 }} />
     </>
   );
 };
 
 export default ScaleResult;
+
+const BackGroundColor = css`
+  background-color: ${variables.colors.gray5};
+  margin: -1.8rem -1.8rem -5rem;
+  padding: ${variables.headerHeight} 1.8rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  gap: 2.2rem;
+`;
+
+const Title = css`
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+  & h2 {
+    font-size: 1.8rem;
+    font-weight: 700;
+  }
+
+  & p {
+    font-size: 1.6rem;
+    color: ${variables.colors.gray70};
+  }
+`;
